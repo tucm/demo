2ef83fb1ceec96d5f9c0299a59cf88e08b7cb61c
tucm <tucm@11.com>
2020-09-16 19:42:05 +0800

    (*)

--- base/src/main/java/com/test/tucm/base/comparetorable/DomainComparator.java
+++ base/src/main/java/com/test/tucm/base/comparetorable/DomainComparator.java
@@ -1,22 +1,18 @@
 package com.test.tucm.base.comparetorable;
 
-import java.util.Comparator;
+public class DomainComparator {
 
-/**
- * @ClassName DomainComparator
- * @Description TODO
- * @Author TUCM
- * @Date 2019-07-06 19:48
- * @Version 1.0
- **/
-public class DomainComparator implements Comparator<DomainComparable> {
-    @Override
-    public int compare(DomainComparable domain1, DomainComparable domain2) {
-        if (domain1.getStr().compareTo(domain2.getStr()) > 0)
-            return 1;
-        else if (domain1.getStr().compareTo(domain2.getStr()) == 0)
-            return 0;
-        else
-            return -1;
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public DomainComparator(String str) {
+        this.str = str;
     }
 }
